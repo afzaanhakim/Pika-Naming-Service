00c417b934e95035be954ca9b56fafd16d253a9f
afzaanhakim <afzaanhakim19@gmail.com>
2022-02-20 15:08:31 -0500

    implemting price logic, maindomain name

--- contracts/Domains.sol
+++ contracts/Domains.sol
@@ -3,26 +3,47 @@
 
 pragma solidity ^0.8.10;
 
+import { StringUtils } from "./libraries/StringUtils.sol";
 import "hardhat/console.sol";
 
 contract Domains {
+  string public maindomain; 
   mapping(string => address) public domains; //storing domain names in a mapping that maps two values in this case i will map domain name which is a string to the wallet address
   mapping(string => string) public records; //storing values i.e records of registrations
-
   mapping(string => string) public emails; //storing emails 
-
   mapping (string => string) public twitter; //storing twitter handles 
-
   mapping (string => string) public professions; //storing profession
   mapping (string => string) public pfp; //storing profile picture
-  constructor() {
+  constructor(string memory _maindomain) payable {
+    maindomain = _maindomain;
+    console.log("%s name service deployed", _maindomain);
     console.log("PIKA CONTRACT");
   }
 
-  function register (string calldata name) public { //a register function that takes name as parameter and assignes it to whoeevr calls this func on the chain (msg.sender) and adds the name to the domains mapping - calldata is the location of the where name argument should be stored. I am storing in calldata so it takes less gas.
-  require(domains[name] == address(0)); //checking to see if name is unregistered i.e there are 0 address for that name inside the domains mapping
+  //a function for price of domain based on length of characters in domain
+
+  function price(string calldata name) public pure returns(uint){
+    uint len = StringUtils.strlen(name);
+    require(len > 0);
+    if(len == 3) {
+      return 1 * 10**17; // 5 MATIC = 5 000 000 000 000 000 000 (18 decimals). 0.5 Matic cause the faucets don't give a lot
+    }
+    else if(len == 4) {
+      return 0.5 * 10**17; //reducing price if characters are higher
+    }
+    else {
+      return 0.3 * 10**17;
+    }
+  }
+
+  function register (string calldata name) public payable { //a register function that takes name as parameter and assignes it to whoeevr calls this func on the chain (msg.sender) and adds the name to the domains mapping - calldata is the location of the where name argument should be stored. I am storing in calldata so it takes less gas.
+    require(domains[name] == address(0));//checking to see if name is unregistered i.e there are 0 address for that name inside the domains mapping
+    uint _price = price(name); //calculating price of the name used
+   // Check if enough Matic was paid in the transaction
+    require(msg.value >= _price, "Not enough Matic paid"); //checking if value of transaction is greater or equal to the price paid
     domains[name] = msg.sender; 
     console.log("%s has register a domain", msg.sender);
+    console.log("%s has cost", domains[name], _price);
   }
 
   function getAddress(string calldata name) public view returns(address) { //simple function that returns the address of the owner of a specific name
@@ -41,7 +62,6 @@ contract Domains {
   function setEmail(string calldata name, string calldata email) public {
     require(domains[name] == msg.sender);
     emails[name] = email; //setting emails of the person registered to this domain
-    console.log("%s has register an email", msg.sender, emails[name]);
   }
 
   function getEmail(string calldata name) public view returns(string memory){
@@ -51,7 +71,6 @@ contract Domains {
   function setTwitter(string calldata name, string calldata handle) public {
      require(domains[name] == msg.sender);
      twitter[name] = handle;
-     console.log("%s has register a twitter", msg.sender, twitter[name]);
   }
 
   function getTwitter(string calldata name) public view returns(string memory){
@@ -61,7 +80,6 @@ contract Domains {
   function setProfession(string calldata name, string calldata profession) public {
     require(domains[name] == msg.sender);
     professions[name] = profession;
-    console.log("%s has register a profession", msg.sender, professions[name]);
   }
 
   function getProfession(string calldata name) public view returns(string memory) {
@@ -71,7 +89,6 @@ contract Domains {
   function setPFP (string calldata name, string calldata pic) public {
     require(domains[name] == msg.sender);
     pfp[name] = pic;
-    console.log("%s has submitted a pfp link", msg.sender, pfp[name]);
   }
 
   function getPFP(string calldata name) public view returns(string memory){
--- scripts/run.js
+++ scripts/run.js
@@ -1,32 +1,35 @@
 const main = async () => {
   const [owner, randomPerson] = await hre.ethers.getSigners(); //owner of the contract and random address
   const domainContractFactory = await hre.ethers.getContractFactory("Domains"); //compiling Domain from our contracts folder using hardhat runtime environment
-  const domainContract = await domainContractFactory.deploy(); //creating a local eth network for the contract and destroying the network after script is complete
+  const domainContract = await domainContractFactory.deploy("pika"); //creating a local eth network for the contract and destroying the network after script is complete also passing pika as the maindomain into the constructor of the domains contratc
   await domainContract.deployed();
 
   console.log("deployed to:", domainContract.address);
-  console.log("deployed by:", owner.address);
-  let txn = await domainContract.register("pika"); //registering "pika"
+
+  let txn = await domainContract.register("trouble", {value: hre.ethers.utils.parseEther('0.1')}); //registering "trouble"
   await txn.wait();
 
   
 
-  const domainOwnerAddress = await domainContract.getAddress("pika");
-  console.log("Owner of `pika` ", domainOwnerAddress);
+  const domainOwnerAddress = await domainContract.getAddress("trouble");
+  console.log("Owner of `trouble` ", domainOwnerAddress);
+
+  const balance = await hre.ethers.provider.getBalance(domainContract.address);
+  console.log("Contract balance for Pika Naming Service", hre.ethers.utils.formatEther(balance));
 
   // Trying to set a email 
-  txn = await domainContract.connect(owner).setEmail("pika","123@fraud.com");
+  txn = await domainContract.connect(owner).setEmail("trouble","trouble@fraud.com");
   await txn.wait();
 
   //trying to set twitter handle
-  txn = await domainContract.connect(owner).setTwitter("pika", "@fraud");
+  txn = await domainContract.connect(owner).setTwitter("trouble", "@troublesome");
   await txn.wait();
 
   //trying to set profession
-  txn = await domainContract.connect(owner).setProfession("pika", "dev");
+  txn = await domainContract.connect(owner).setProfession("trouble", "memeartist");
   await txn.wait();
 //set a display pic
-  txn = await domainContract.connect(owner).setPFP("pika", "https://cloudflare-ipfs.com/ipfs/QmdJZjrrHuWZJ8RWuKGjSSc17fYLS8jPJrR3CWzES4Lxns");
+  txn = await domainContract.connect(owner).setPFP("trouble", "https://cloudflare-ipfs.com/ipfs/QmUKHB3EZmeakGHyQDmpSHBxUuYTDjG45Yksq6XXU2RiaG");
   await txn.wait();
 };
 
